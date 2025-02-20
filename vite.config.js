3ed7d08a3e164d760e979976d12681c6ca3b356b
dogglepiyush <131369719+PIYushK0916@users.noreply.github.com>
2025-02-20 15:21:11 +0530

    images correction

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'  // Base URL for GitHub Pages
+  base: '/', // Change this if deploying to a subdirectory
 });
